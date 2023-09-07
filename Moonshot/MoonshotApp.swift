045dada6e6ad5ff3cc62621b79bed53631eed8ed
Marcio Curvello <marciocurvello@gmail.com>
2023-09-06 23:53:32 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Marcio Curvello on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
